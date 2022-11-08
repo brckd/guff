30a8361fb2c93e071db3a1ace988f8f507d09035
Brycked <brycked@outlook.com>
2022-11-08 20:01:26 +0100

    ignore dist/

--- dist/core/modules/Module.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Module = void 0;
-/** Base class for a module */
-class Module {
-}
-exports.Module = Module;
--- dist/core/modules/Precondition.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Precondition = void 0;
-const Module_1 = require("./Module");
-class Precondition extends Module_1.Module {
-}
-exports.Precondition = Precondition;
